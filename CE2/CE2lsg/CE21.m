9a3d8736d17fc4442826c0f8d38fb031f316b5aa
Jonathan Olsson <jonathan.olsson8@gmail.com>
2024-10-05 00:34:23 +0200

    creating new branch

--- CE2/CE2lsg/CE21.m
+++ CE2/CE2lsg/CE21.m
@@ -8,16 +8,3 @@ u=randn(1000,1);
 d=conv(h,u);
 % LMS
 [e,w]=lms(0.1,5,u,d);
-
-function [e,w] = lms(mu,M,u,d)
-    wcalc(M);
-    function w_next = wcalc(n)
-        if n <= 0
-            w_next = 0;
-        else
-            w_past = wcalc(n-1);
-            e = d-w_past'*u;
-            w = w_past+mu*u*e;
-        end
-    end
-end
